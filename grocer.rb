38ba28eeee9a0e446d9cf82df84dfcb36d4ef7b2
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-26 23:57:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -109,12 +109,16 @@ def checkout(cart, coupons)
   
   final_cart = consolidate_cart(cart)
   final_cart = apply_coupons(final_cart, coupons)
+  pp final_cart
   final_cart = apply_clearance(final_cart)
+  pp final_cart
   
   while final_cart[count] do
     total += final_cart[count][:price] * final_cart[count][:count]
     count += 1
   end
+  
+  total *= 0.9 if total >= 100
   
   total
 end
